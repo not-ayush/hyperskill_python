3b74102043504be2102074cc6a645ca8dda87fe4
aay <gangwarayush921@gmail.com>
2023-05-11 17:42:03 +0530

    added class to stage 3

--- tictactoe_w_ai/stage_3.py
+++ tictactoe_w_ai/stage_3.py
@@ -1,18 +1,66 @@
 import random
-#  in this stage, added the ability to choose player modes
-board = [
-    [' ', ' ', ' '],
-    [' ', ' ', ' '],
-    [' ', ' ', ' ']]
 
 
-def print_board():
-    print(f'''
----------
-| {board[0][0]} {board[0][1]} {board[0][2]} |
-| {board[1][0]} {board[1][1]} {board[1][2]} |
-| {board[2][0]} {board[2][1]} {board[2][2]} |
----------''')
+class Board:
+    grid = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+
+    def __int__(self):
+        self.grid = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+
+    def print_board(self):
+        print(f'''
+    ---------
+    | {self.grid[0][0]} {self.grid[0][1]} {self.grid[0][2]} |
+    | {self.grid[1][0]} {self.grid[1][1]} {self.grid[1][2]} |
+    | {self.grid[2][0]} {self.grid[2][1]} {self.grid[2][2]} |
+    ---------''')
+
+    def check_win(self):  # returns the mark that won, or None
+        winner = None
+        # rows, columns
+        for r in range(3):
+            if self.grid[r][0] == self.grid[r][1] == self.grid[r][2] == 'X':
+                winner = 'X'
+            elif self.grid[r][0] == self.grid[r][1] == self.grid[r][2] == 'O':
+                winner = 'O'
+        for c in range(3):
+            if self.grid[0][c] == self.grid[1][c] == self.grid[2][c] == 'X':
+                winner = 'X'
+            elif self.grid[0][c] == self.grid[1][c] == self.grid[2][c] == 'O':
+                winner = 'O'
+        # diagonals
+        if (self.grid[0][0] == self.grid[1][1] == self.grid[2][2] == 'X') or (
+                self.grid[0][2] == self.grid[1][1] == self.grid[2][0] == 'X'):
+            winner = 'X'
+        elif (self.grid[0][0] == self.grid[1][1] == self.grid[2][2] == 'O') or (
+                self.grid[0][2] == self.grid[1][1] == self.grid[2][0] == 'O'):
+            winner = 'O'
+        return winner
+
+    def check_empty(self):  # return true if
+        empty = False
+        for row in self.grid:
+            for elem in row:
+                if elem == ' ':
+                    empty = True
+        return empty
+
+    def check_game_state(self):
+        if not self.check_win() and not self.check_empty():
+            return 'Draw'
+        elif self.check_win():
+            return f'{self.check_win()} wins'
+
+    def return_empty_places(self):
+        empty_indices = []
+        for r in range(3):
+            for c in range(3):
+                if self.grid[r][c] == ' ':
+                    empty_indices.append((r, c))
+        return empty_indices
+
+
+board = Board()
 
 
 def make_users_move(mark: str):  # asks what move, check correct move, makes user's move
@@ -20,16 +68,16 @@ def make_users_move(mark: str):  # asks what move, check correct move, makes use
     while not check_valid_move(coord_inp):
         coord_inp = input('Enter the coordinates: ')
     coords = coord_inp.split(' ')
-    board[int(coords[0]) - 1][int(coords[1]) - 1] = mark
+    board.grid[int(coords[0]) - 1][int(coords[1]) - 1] = mark
 
 
-def make_comp_move(mark: str):
-    comp_choice = random.choice(return_empty_places())
+def make_easy_move(mark: str):
+    comp_choice = random.choice(board.return_empty_places())
     print('Making move level "easy"')
-    board[comp_choice[0]][comp_choice[1]] = mark
+    board.grid[comp_choice[0]][comp_choice[1]] = mark
 
 
-move_funcs = {'user': make_users_move, 'easy': make_comp_move}
+move_funcs = {'user': make_users_move, 'easy': make_easy_move}
 
 
 def check_valid_move(coords_: str):
@@ -37,56 +85,10 @@ def check_valid_move(coords_: str):
         print('You should enter numbers!')
     elif coords_[0] not in '123' or coords_[2] not in '123':
         print('Coordinates should be from 1 to 3!')
-    elif board[int(coords_[0]) - 1][int(coords_[2]) - 1] != ' ':
+    elif board.grid[int(coords_[0]) - 1][int(coords_[2]) - 1] != ' ':
         print('This cell is occupied! Choose another one!')
     else:
         return True
-
-
-def check_win():  # returns the mark that won, or None
-    winner = None
-    # rows, columns
-    for r in range(3):
-        if board[r][0] == board[r][1] == board[r][2] == 'X':
-            winner = 'X'
-        elif board[r][0] == board[r][1] == board[r][2] == 'O':
-            winner = 'O'
-    for c in range(3):
-        if board[0][c] == board[1][c] == board[2][c] == 'X':
-            winner = 'X'
-        elif board[0][c] == board[1][c] == board[2][c] == 'O':
-            winner = 'O'
-    # diagonals
-    if (board[0][0] == board[1][1] == board[2][2] == 'X') or (board[0][2] == board[1][1] == board[2][0] == 'X'):
-        winner = 'X'
-    elif (board[0][0] == board[1][1] == board[2][2] == 'O') or (board[0][2] == board[1][1] == board[2][0] == 'O'):
-        winner = 'O'
-    return winner
-
-
-def check_empty():  # return true if
-    empty = False
-    for row in board:
-        for elem in row:
-            if elem == ' ':
-                empty = True
-    return empty
-
-
-def check_game_state():
-    if not check_win() and not check_empty():
-        return 'Draw'
-    elif check_win():
-        return f'{check_win()} wins'
-
-
-def return_empty_places():
-    empty_indices = []
-    for r in range(3):
-        for c in range(3):
-            if board[r][c] == ' ':
-                empty_indices.append((r, c))
-    return empty_indices
 
 
 def player_mode_check():  # check players and update variables
@@ -121,19 +123,20 @@ while inp_comm != 'exit':
         break
     player_mode_check()
     # start game
-    print_board()
+    board.print_board()
     for i in range(9):
         if i % 2 == 0:  # player 1 turn 'X'
+            # make_users_move()
             move_funcs[player_1]('X')
-            print_board()
-            game_state = check_game_state()
+            board.print_board()
+            game_state = board.check_game_state()
             if game_state:
                 print(game_state)
                 break
         else:  # player 2 turn  'O'
             move_funcs[player_2]('O')
-            print_board()
-            game_state = check_game_state()
+            board.print_board()
+            game_state = board.check_game_state()
             if game_state:
                 print(game_state)
                 break
